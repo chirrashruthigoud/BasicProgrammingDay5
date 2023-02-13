454f2141bc15c27c2fb4445ede797dc5fe1466c2
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-13 12:37:55 +0530

    Basic practice problems

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Logicalpoblems;
+
+namespace Logicalpoblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EvenorOdd.CheckEvenorOdd();
+           // QuotientAndRemainder.FindQuotientAndRemainder();
+            //LargestNumber.FindLargestNumber();
+            // Swapping.SwappingtheNumber();
+            //Vowelandconsonant.Checkthecharacter();
+        }
+    }
+}
